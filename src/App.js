55edc30b4fab1352258a62b20a379251e3ac0be9
MrLucha <mrluchadev@hotmail.com>
2023-07-25 12:49:36 -0600

    add changes to render lists

--- src/App.js
+++ src/App.js
@@ -8,8 +8,14 @@ function App() {
   const [sentencia, setSentencia] = useState('')
   //estado para actualizar el text area
   const [text, setText] = useState('')
+  //estado para el objeto del backend
+  const [respuesta, setRespuesta] = useState('')
+  const [tokens, setTokens] = useState(false)
+  const [sintaxis, setSintaxis] = useState(false)
+  const [semantica, setSemantica] = useState(false)
+  const [traduccion, setTraduccion] = useState('')
   //Variable para cambiar el backend
-  const url = 'http://localhost:8000/ruta-del-backend'
+  const URI = process.env.REACT_APP_URI;
   //estado para la respuesta del backend
   const [respuestaBackend, setRespuestaBackend] = useState(null); 
 
@@ -19,7 +25,27 @@ function App() {
 
 
   const traducir = async () => {
-    setText(sentencia)
+    const res = await fetch(`${URI}/`,{
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        sentencia
+      })
+    })
+    const data = await res.json()
+    setRespuesta(data)
+    setTokens(data[0])
+    setSintaxis(data[1])
+    setSemantica(data[2])
+    setTraduccion(data[3])
+
+    // if(data[3]!="No disponible"){
+    //   setText(sentencia)
+    // }
+    
+    console.log(data)
     
 
 
@@ -39,9 +65,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Traductor inglés a español</h1>
+        <h1>Traductor inglés a español presente simple</h1>
         <div className="centered-container">
-          <p>Contenido del contenedor centrado.</p>
+          <h3>Estructura correcta de las oraciones</h3>
+          <h6>Affirmative → Sujeto + Verbo + Complemento </h6>
+          <h6>Negative → Sujeto + Negacion + Verbo + Complemento</h6>
+          <h6>Question → Verbo auxiliar + Sujeto + Verbo + Complemento</h6>
           <div className="custom-search">
             <input 
               type="text" 
@@ -60,10 +89,70 @@ function App() {
           </div>
           <textarea 
             className='custom-textarea'
-            rows={6}
+            rows={2}
             readOnly
-            value={text}
+            value={traduccion}
           />
+
+          {
+            tokens ? (
+              <div  className="centrado">
+                <hr />
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Tipo de palabra</th>
+                      <th>palabra</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {tokens.map((lista, index) => (
+                      <tr key={index}>
+                        <td>{lista[1]}</td>
+                        <td>{lista[0]}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div> 
+            ) : (
+              <h3></h3>
+            )
+
+
+          }
+
+          {
+
+            sintaxis ? (
+
+              <div>
+              <hr />
+              <h2>{sintaxis}</h2>
+              </div> 
+
+            ) : (
+              <h3></h3>
+            )
+
+          }
+
+          {
+
+          semantica ? (
+
+            <div>
+            <hr />
+            <h2>{semantica}</h2>
+            </div> 
+
+          ) : (
+            <h3></h3>
+          )
+
+          }
+
+
         </div>
       </header>
     </div>
